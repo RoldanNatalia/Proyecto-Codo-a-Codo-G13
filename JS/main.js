0d0170e4d627a7a8255f2355ee19ddeca0993fca
Natalia Roldan <111518783+RoldanNatalia@users.noreply.github.com>
2022-10-21 19:24:03 -0300

    formulario

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,48 @@
+function validarForm(){
+          
+  let isCorrect=true;
+  
+  
+  if(document.getElementById("InputName").value.length < 2 ){
+    
+    isCorrect = false;
+    
+  }
+   
+  
+  if(document.getElementById("InputEmail1").value.length < 5 ){
+      isCorrect = false;
+  }
+      if (isCorrect){
+        
+          let nom=document.getElementById("InputName").value
+          let email=document.getElementById("InputEmail1").value
+          localStorage.setItem("Nombre",nom)
+          localStorage.setItem("email", email)
+          
+
+         /* const fs = require('fs'); 
+ 
+          let objectToSave = {nombre:nom,email:email} 
+ 
+          fs.writeFile('archivo.json', JSON.stringify(objectToSave),'utf8', (err) => { 
+          if (err) throw err; 
+          console.log('The file has been saved!'); 
+          }); */
+
+          }
+      
+    
+  
+      return isCorrect;
+    
+
+          
+  
+}
+
+
+
 /*const { createApp } = Vue
 
   createApp({
@@ -27,22 +72,19 @@
                 <div class="cuerpo">
                     <h4>${producto.id} - ${producto.title}</h4>
                     <p>${producto.description}</p>
-                    
+                    <a class="boton" href=${producto.spotify}>IR A </a>
                 </div>
             </div>   
         `
             }
     
     
-            cad += `</div>
-             `
+            cad += `</div>`
     
             console.log(cad)
     
-    
-    
-            document.getElementById("tarjetas").innerHTML = cad;
+            document.getElementById("tarjetas").innerHTML= cad;
     
     
         }
-        );
+      );
